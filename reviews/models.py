3b99119485de3cc5f597094f1803a2ddc5089a61
theRealCooller <jakublohnicky@hotmail.com>
2023-11-14 17:57:15 +0100

    add tests for models

--- reviews/models.py
+++ reviews/models.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Optional
+from typing import List, Tuple
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
