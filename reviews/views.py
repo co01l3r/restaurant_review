6ecf7c2c857d81a2e28c01676c6b6eefafef97e1
theRealCooller <jakublohnicky@hotmail.com>
2023-11-16 23:53:14 +0100

    enable js

--- reviews/views.py
+++ reviews/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
 from django.db import DatabaseError
-from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import (LoginForm, RegistrationForm, RestaurantForm, ReviewForm,
